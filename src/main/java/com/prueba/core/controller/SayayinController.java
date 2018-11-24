0c9a700797a9062c1fab5189cb61cdfb6bf0f2a6
unknown <bogginice@gmail.com>
2018-11-24 12:43:10 -0300

    act 23-nov-2018

--- src/main/java/com/prueba/core/controller/SayayinController.java
+++ src/main/java/com/prueba/core/controller/SayayinController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prueba.core.model.Sayayin;
@@ -27,11 +29,12 @@ public class SayayinController
 	SayayinRepo repo;
 	
 	@DeleteMapping("/sayayins/{id}")
-	public String deleteSayayin(@PathVariable int id)
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteSayayin(@PathVariable int id)
 	{
 		Sayayin s = repo.getOne(id);
 		repo.delete(s);
-		return  s.getName()+ " borrado";
+
 	}
 	
 	@PostMapping("/sayayins")
